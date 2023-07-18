e70d5f181bfffb1c0c0e68b3888fb838f8ab2ac4
unknown <denis_devillers_7@hotmail.com>
2023-07-18 17:22:52 +0200

    18 july ajout favicon

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="shortcut icon"  href="{{asset('assets/img/logo.png')}}">
     <title>@yield('title')</title>
     <!-- Fonts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
